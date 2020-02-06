c969dd3880feb8848f1aae992aad5f44e0b81594
AzaniaBG <azaniazbaker@gmail.com>
2020-02-06 12:00:08 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import './App.css';
+import STORE from './Store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="App">   
+        <header className="App-header">
+          {props.children}
+          <div className="App-list">
+            <STORE>
+              
+            </STORE>
+          </div>
+        </header>     
+    </main>
+  )
 }
 
 export default App;
